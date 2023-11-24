4906aaf1f79dbeec951c796f524fa8bb21ebb03d
GMwanda <George.Mwanda@strathmore.edu>
2023-11-24 21:50:10 +0300

    Complete 2048 game

--- main.py
+++ main.py
@@ -10,6 +10,12 @@ HEIGHT = 500
 timer = pygame.time.Clock()
 FPS = 60
 FONT = pygame.font.Font('freesansbold.ttf', 24)
+direction = ''
+score = 0
+file = open('high_score', 'r')
+initial_high = int(file.readline())
+file.close()
+high_score = initial_high
 
 COLORS = {
     0: (204, 192, 179),
@@ -17,6 +23,8 @@ COLORS = {
     4: (237, 224, 200),
     8: (242, 177, 121),
     16: (245, 149, 99),
+    32: (246, 124, 95),
+    64: (246, 94, 59),
     128: (237, 207, 114),
     256: (237, 204, 97),
     512: (237, 200, 80),
@@ -35,11 +43,15 @@ init_count = 0
 
 # SCREEN SETUP
 screen = pygame.display.set_mode([WIDTH, HEIGHT])
-pygame.display.set_caption('2048')
+pygame.display.set_caption('Josiah\'s 2048')
 
 
 def draw_board():
     pygame.draw.rect(screen, COLORS['bg'], [0, 0, 400, 400], 0, 10)
+    score_text = FONT.render(f'Score: {score}', True, 'black')
+    high_score_text = FONT.render(f'High Score: {high_score}', True, 'black')
+    screen.blit(score_text, (10, 410))
+    screen.blit(high_score_text, (10, 450))
 
 
 def draw_pieces(board_values):
@@ -84,9 +96,111 @@ def new_pieces(board_values):
     return board_values, full
 
 
+def take_turn(direction, board):
+    merged = [[False for _ in range(4)] for _ in range(4)]
+    if direction == 'UP':
+        take_turn_up(board, merged)
+    elif direction == 'DOWN':
+        take_turn_down(board, merged)
+    elif direction == 'LEFT':
+        take_turn_left(board, merged)
+    elif direction == 'RIGHT':
+        take_turn_right(board, merged)
+
+    return board
+
+
+def take_turn_up(board, merged):
+    global score
+    for i in range(4):
+        for j in range(4):
+            shift = 0
+            if i > 0:
+                for k in range(i):
+                    if board[k][j] == 0:
+                        shift += 1
+                if shift > 0:
+                    board[i - shift][j] = board[i][j]
+                    board[i][j] = 0
+                if board[i - shift - 1][j] == board[i - shift][j] and not merged[i - shift - 1][j] and not \
+                        merged[i - shift][j]:
+                    board[i - shift - 1][j] *= 2
+                    score += board[i - shift - 1][j]
+                    board[i - shift][j] = 0
+                    merged[i - shift - 1][j] = True
+
+
+def take_turn_down(board, merged):
+    global score
+    for i in range(3):
+        for j in range(4):
+            shift = 0
+            for q in range(i + 1):
+                if board[3 - q][j] == 0:
+                    shift += 1
+            if shift > 0:
+                board[2 - i + shift][j] = board[2 - i][j]
+                board[2 - i][j] = 0
+            if 3 - i + shift <= 3:
+                if board[2 - i + shift][j] == board[3 - i + shift][j] and not merged[3 - i + shift][j] and not \
+                        merged[2 - i + shift][j]:
+                    board[3 - i + shift][j] *= 2
+                    score += board[3 - i + shift][j]
+                    board[2 - i + shift][j] = 0
+                    merged[3 - i + shift][j] = True
+
+
+def take_turn_left(board, merged):
+    global score
+    for i in range(4):
+        for j in range(4):
+            shift = 0
+            for q in range(j):
+                if board[i][q] == 0:
+                    shift += 1
+            if shift > 0:
+                board[i][j - shift] = board[i][j]
+                board[i][j] = 0
+            if board[i][j - shift] == board[i][j - shift - 1] and not merged[i][j - shift - 1] and not merged[i][
+                j - shift]:
+                board[i][j - shift - 1] *= 2
+                score += board[i][j - shift - 1]
+                board[i][j - shift] = 0
+                merged[i][j - shift - 1] = True
+
+
+def take_turn_right(board, merged):
+    global score
+    for i in range(4):
+        for j in range(4):
+            shift = 0
+            for q in range(j):
+                if board[i][3 - q] == 0:
+                    shift += 1
+            if shift > 0:
+                board[i][3 - j + shift] = board[i][3 - j]
+                board[i][3 - j] = 0
+            if 4 - j + shift <= 3:
+                if board[i][4 - j + shift] == board[i][3 - j + shift] and not merged[i][4 - j + shift] and not \
+                        merged[i][
+                            3 - j + shift]:
+                    board[i][4 - j + shift] *= 2
+                    score += board[i][4 - j + shift]
+                    board[i][4 - j + shift] = 0
+                    merged[i][4 - j + shift] = True
+
+
+def draw_game_over():
+    pygame.draw.rect(screen, 'black', [50, 50, 300, 100], 0, 10)
+    game_over_text1 = FONT.render("Game Over!!", True, 'white')
+    game_over_text2 = FONT.render("Press Enter to Restart", True, 'white')
+    screen.blit(game_over_text1, (130, 65))
+    screen.blit(game_over_text2, (70, 105))
+
+
 # GAME LOOP
 def game_loop():
-    global spawn_new, init_count
+    global spawn_new, init_count, direction, board_values, high_score, game_over, initial_high, score, BOARD_VALUES
     running = True
     while running:
         timer.tick(FPS)
@@ -99,13 +213,44 @@ def game_loop():
             spawn_new = False
             init_count += 1
 
+        if direction != '':
+            board_values = take_turn(direction, board_values)
+            direction = ''
+            spawn_new = True
+
+        if game_over:
+            draw_game_over()
+            if high_score > initial_high:
+                file = open('high_score', 'w')
+                file.write(f'{high_score}')
+                file.close()
+                initial_high = high_score
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
 
-            if event.type == pygame.KEYDOWN:
+            if event.type == pygame.KEYUP:
+                if event.key == pygame.K_UP:
+                    direction = 'UP'
+                elif event.key == pygame.K_DOWN:
+                    direction = 'DOWN'
+                elif event.key == pygame.K_RIGHT:
+                    direction = 'RIGHT'
+                elif event.key == pygame.K_LEFT:
+                    direction = 'LEFT'
 
+                if game_over:
+                    if event.key == pygame.K_RETURN:
+                        BOARD_VALUES = [[0 for _ in range(4)] for _ in range(4)]
+                        spawn_new = True
+                        init_count = 0
+                        score = 0
+                        direction = ''
+                        game_over = False
 
+        if score > high_score:
+            high_score = score
         pygame.display.flip()
     pygame.quit()
 
